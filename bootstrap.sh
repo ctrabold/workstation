6f131444d40304ca20b50cbc85f78d50fd94bb2a
Christian Trabold <info@christian-trabold.de>
2014-04-11 20:42:43 +0700

    Improve ubuntu support
    repos get only registered once.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,13 @@ echo "INFO Installing ansible..."
 if [[ $OSTYPE == darwin* ]]; then
   brew install ansible
 else
-  sudo add-apt-repository -y ppa:rquillo/ansible
-  sudo apt-get install -y ansible
+  repo_installed=`grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep rquillo/ansible`
+  if [[ repo_installed ]]; then
+    echo "Ansible is already installed"
+  else
+    sudo add-apt-repository -y ppa:rquillo/ansible
+    sudo apt-get install -y ansible
+  fi
 fi
 
 # Run ansible
