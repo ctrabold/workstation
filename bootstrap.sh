d120e078a015e1d75bdb7c3c9de427491f360bb0
Christian Trabold <info@christian-trabold.de>
2014-04-11 20:22:46 +0700

    Resolve todo on supporting multiple platforms

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# TODO:
-# Check which python
-# Get operation system Ubuntu / Mac
-# install ansible from repo / brew?
+set -e
+
+# Smoketest environment
+if [[ `which python` ]]; then
+  echo "OK Found python!"
+fi
+
+# Installing most important package
+echo "INFO Installing ansible..."
+if [[ $OSTYPE == darwin* ]]; then
+  brew install ansible
+else
+  sudo add-apt-repository -y ppa:rquillo/ansible
+  sudo apt-get install -y ansible
+fi
+
 # Run ansible
-echo "Let's go!"
+echo "INFO Execute ansible playbook"
 ansible-playbook -vvvv -i ./hosts.ini -K playbook.yml --extra-vars "login_user=$USER ansible_hostname=`hostname -f`"
