ded40a3685850d6fecae32b762a8af4a9e612d55
Christian Trabold <ctrabold@thoughtworks.com>
2016-02-05 02:48:05 +0800

    bootstrap: Run in non-interactive mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,8 +27,8 @@ else
   if [[ $OSTYPE == darwin* ]]; then
     brew install ansible
   else
-    sudo apt-get install software-properties-common
-    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt-get update
     sudo apt-get install -y ansible
   fi
