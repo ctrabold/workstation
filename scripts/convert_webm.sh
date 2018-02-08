13bfe060df15d6694e581624744af80d02bc8b0b
Christian Trabold <ctrabold@thoughtworks.com>
2018-02-08 17:21:19 +0800

    scripts: Fix syntax error

--- scripts/convert_webm.sh
+++ scripts/convert_webm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for FILE in ls *.webm *.mkv; do
+for FILE in *.webm *.mkv; do
   echo $FILE
   # ffmpeg -i "$FILE" -b:a 128k -vcodec mpeg4 -b:v 1200k -flags +aic+mv4 "${FILE}.mp4"
   avconv -i "$FILE" "${FILE}.mp4"
